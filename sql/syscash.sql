8caff1f033d1139877314f778f08120f06634717
Bruno Karl Ramlow <bruno.ramlow02@gmail.com>
2023-06-25 21:46:13 -0300

    syscash

---
+++ sql/syscash.sql
@@ -0,0 +1,207 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 26/06/2023 às 02:33
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `syscash`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(45) NOT NULL,
+  `tipo` smallint(6) NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `conta_pagar`
+--
+
+CREATE TABLE `conta_pagar` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(100) NOT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `data_vencimento` date NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `id_favorecido` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `conta_receber`
+--
+
+CREATE TABLE `conta_receber` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(100) NOT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `data_vencimento` date NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `id_favorecido` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `favorecido`
+--
+
+CREATE TABLE `favorecido` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `tipo` smallint(6) NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `senha` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_categoria_usuario` (`usuario_id`);
+
+--
+-- Índices de tabela `conta_pagar`
+--
+ALTER TABLE `conta_pagar`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_conta_pagar_categoria` (`categoria_id`),
+  ADD KEY `fk_conta_pagar_usuario` (`usuario_id`),
+  ADD KEY `id_favorecido` (`id_favorecido`);
+
+--
+-- Índices de tabela `conta_receber`
+--
+ALTER TABLE `conta_receber`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_conta_receber_categoria` (`categoria_id`),
+  ADD KEY `fk_conta_receber_usuario` (`usuario_id`),
+  ADD KEY `fk_id_favorecido` (`id_favorecido`);
+
+--
+-- Índices de tabela `favorecido`
+--
+ALTER TABLE `favorecido`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_favorecido_usuario` (`usuario_id`);
+
+--
+-- Índices de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `conta_pagar`
+--
+ALTER TABLE `conta_pagar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `conta_receber`
+--
+ALTER TABLE `conta_receber`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `favorecido`
+--
+ALTER TABLE `favorecido`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `categoria`
+--
+ALTER TABLE `categoria`
+  ADD CONSTRAINT `fk_categoria_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`);
+
+--
+-- Restrições para tabelas `conta_pagar`
+--
+ALTER TABLE `conta_pagar`
+  ADD CONSTRAINT `fk_conta_pagar_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`),
+  ADD CONSTRAINT `fk_conta_pagar_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`),
+  ADD CONSTRAINT `id_favorecido` FOREIGN KEY (`id_favorecido`) REFERENCES `favorecido` (`id`);
+
+--
+-- Restrições para tabelas `conta_receber`
+--
+ALTER TABLE `conta_receber`
+  ADD CONSTRAINT `fk_conta_receber_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`),
+  ADD CONSTRAINT `fk_conta_receber_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`),
+  ADD CONSTRAINT `fk_id_favorecido` FOREIGN KEY (`id_favorecido`) REFERENCES `favorecido` (`id`);
+
+--
+-- Restrições para tabelas `favorecido`
+--
+ALTER TABLE `favorecido`
+  ADD CONSTRAINT `fk_favorecido_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
